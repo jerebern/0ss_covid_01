ed3b2984cb823c9dca093ff0505b9f7eff1c3937
Jérémy Bernard <jere.bern@hotmail.com>
2020-04-06 14:17:01 -0400

    Numero 3.2

---
+++ BusinessRules.Model/IDataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessRules.Model
+{
+    interface IDataService<T>
+    {
+        IEnumerable<T> GetAll();
+    }
+
+}
